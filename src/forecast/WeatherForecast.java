3bad215ea0a4fc1d791b7dd9291e0873395dd84e
Carleigh <carleigh.duncan@gmail.com>
2021-03-17 09:06:28 -0400

    removed a weird line

--- src/forecast/WeatherForecast.java
+++ src/forecast/WeatherForecast.java
@@ -10,7 +10,7 @@ public WeatherForecast(){
 public static void Forecast(Response ex){ // This needs to return the response after editing it so that the window class can print something pretty (TOSTRING!!)
 	Response info = ex;
 	
-	info.getSys().setSunrise(null);
+	//info.getSys()
 	
 }
 
